db7cc1324f2d6c292ad62c0ac17ff3e27212913f
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-12-19 20:19:05 +0530

    ui: turn-website-into-dark-mode_change-font-to-helvetica

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -14,7 +14,7 @@ const Section = (props) => {
     >
       <div className="w-1/2 flex items-center justify-center">
         <div className="max-w-sm w-full">
-          <div className="bg-white  rounded-lg px-8 py-12">
+          <div className="bg-[#2d102f7d] rounded-lg px-8 py-12 text-white">
             {props.children}
           </div>
         </div>
@@ -37,57 +37,56 @@ export const Overlay = () => {
 
   return (
     <Scroll html>
-      <div class="w-screen">
+      <div className="w-screen">
         <Section opacity={opacityFirstSection}>
           <h1 className="font-semibold font-serif text-2xl">
             Hello, I'm Amaan!
           </h1>
-          <p className="text-gray-500">Welcome to my office</p>
+          <p className="text-white">Welcome to my office</p>
           
-          <ul className="leading-9">
+          <ul className="leading-9 text-white">
             <li>Know more about me at <a href="https://amaanbhati.tech" className="text-blue-500 hover:underline">My portfolio</a></li>
             <li> You can read my blogs at <a href="https://blog.amaanbhati.tech" className="text-blue-500 hover:underline">Blog site</a></li>
             <li>You can contact me at <a href="https://contact.amaanbhati.tech" className="text-blue-500 hover:underline">My socials</a></li>
-            
           </ul>
-          <p className="animate-bounce  mt-6">↓</p>
+          <p className="animate-bounce mt-6 text-white">↓</p>
         </Section>
         <Section right opacity={opacitySecondSection}>
-          <h1 className="font-semibold font-serif text-2xl">
+          <h1 className="font-semibold font-serif text-2xl text-white">
             Here are my skillsets 🔥
           </h1>
          
-          <p className="mt-3">
+          <p className="mt-3 text-white">
             <b>Libraries and Frameworks 🚀</b>
           </p>
-          <ul className="leading-9">
+          <ul className="leading-9 text-white">
             <li>ReactJS</li>
             <li>Nextjs</li>
             <li>NodeJS</li>
             <li>TailwindCSS</li>
             <li></li>
           </ul>
-          <p className="mt-3">
+          <p className="mt-3 text-white">
             <b>Languages 🔬</b>
           </p>
-          <ul className="leading-9">
+          <ul className="leading-9 text-white">
             <li>HTML/CSS</li>
             <li>Javascript</li>
             <li>Typescript</li>
             <li>C++</li>
             <li>SQL</li>
           </ul>
-          <p className="animate-bounce  mt-6">↓</p>
+          <p className="animate-bounce mt-6 text-white">↓</p>
         </Section>
         <Section opacity={opacityLastSection}>
-          <h1 className="font-semibold font-serif text-2xl">
+          <h1 className="font-semibold font-serif text-2xl text-white">
             🤙 Contact me maybe?
           </h1>
-          <p className="text-gray-500">
+          <p className="text-white">
             3d development might not be my niche but I can work on it when required 
           </p>
           <p className="mt-6 p-3 bg-slate-200 rounded-lg">
-            📞 <a href="https://contact.amaanbhati.tech">My contact website/linktree</a>
+            📞 <a href="https://contact.amaanbhati.tech" className="text-blue-500 hover:underline">My contact website/linktree</a>
           </p>
         </Section>
       </div>
